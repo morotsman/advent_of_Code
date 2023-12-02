ae719061c978298b7359b76146df74ba27d8565e
Niklas Leopold <leopold.niklas@gmail.com>
2023-12-02 09:58:14 +0100

    refactoring

--- 2022/app/Day7_1.hs
+++ 2022/app/Day7_1.hs
@@ -4,16 +4,10 @@ module Main where
 import Data.List (isInfixOf)
 import Control.Lens
 
-data FileSystem = Directory String
+data Row = Directory String
             | File String Int
-            deriving (Show)
-
-data Command = CD String
+            | CD String
             | LS
-            deriving (Show)
-
-data Row = FileSystemRow FileSystem
-            | CommandRow Command
             deriving (Show)
 
 
@@ -25,7 +19,7 @@ solveIt inputs = do
 
 parseRow :: String -> Maybe Row
 parseRow input | isInfixOf "cd" input = parseCD input
-               | isInfixOf "ls" input = Just(CommandRow (LS))
+               | isInfixOf "ls" input = Just(LS)
                | isInfixOf "dir" input = parseDir input
                | otherwise = parseFile input
 
@@ -33,20 +27,20 @@ parseCD :: String -> Maybe Row
 parseCD input = do
   let columns = words input
   moveTo <- columns ^? element 2
-  return (CommandRow (CD moveTo))
+  return (CD moveTo)
 
 parseDir :: String -> Maybe Row
 parseDir input = do
   let columns = words input
   name <- columns ^? element 1
-  return (FileSystemRow (Directory name))
+  return (Directory name)
 
 parseFile :: String -> Maybe Row
 parseFile input = do
   let columns = words input
   size <- fmap read columns ^? element 0
   name <- columns ^? element 1
-  return (FileSystemRow (File name size))
+  return (File name size)
 
 main :: IO ()
 main = do
