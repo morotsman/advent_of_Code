b2b0b5abce6b01678c01b6d6a0acee2fb38e9ef2
Niklas Leopold <leopold.niklas@gmail.com>
2023-12-14 19:47:22 +0100

    Day 11_1

---
+++ 2022/app/Day11_1.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Control.Lens
+import Day11_example_1 (Monkey(..), getExampleMonkeys)
+
+type Answer = [[Monkey]]
+
+solveIt :: [Monkey] -> Answer
+solveIt = takeTurns 20
+
+takeTurns :: Int -> [Monkey] -> [[Monkey]]
+takeTurns _ [] = []
+takeTurns 0 (m:ms) = []
+takeTurns turn monkeys = let
+  newMonkeys = takeTurn monkeys
+  in [newMonkeys] ++ takeTurns (turn-1) newMonkeys
+
+takeTurn :: [Monkey] -> [Monkey]
+takeTurn monkeys = foldl takeTurnForMonkey monkeys [0..(length monkeys - 1)]
+
+takeTurnForMonkey :: [Monkey] -> Int -> [Monkey]
+takeTurnForMonkey monkeys monkeyIndex | null (startingItems $ monkeys !! monkeyIndex)  = monkeys
+                             | otherwise = let
+  monkey = monkeys !! monkeyIndex
+  (i: is) = startingItems monkey
+  newWorryLevel = (operation monkey i) `div` 3
+  toMonkeyIndex = if (test monkey newWorryLevel) then throwToIfTrue monkey else throwToIfFalse monkey
+  newFromMonkey = monkey { startingItems = is }
+  oldToMonkey = monkeys !! toMonkeyIndex
+  newToMonkey = oldToMonkey { startingItems = newWorryLevel : startingItems oldToMonkey }
+  newMonkeys = replaceMonkey (replaceMonkey monkeys newToMonkey) newFromMonkey
+  in takeTurnForMonkey newMonkeys monkeyIndex
+
+replaceMonkey :: [Monkey] -> Monkey -> [Monkey]
+replaceMonkey monkeys monkey = let
+  index = monkeyIndex monkey
+  (before, _: after) = splitAt index monkeys
+  in before ++ [monkey] ++ after
+
+showMonkeyTurn :: Monkey -> String
+showMonkeyTurn monkey = "Monkey " ++ (show $ monkeyIndex monkey) ++ " has " ++ (show $ startingItems monkey)
+
+main :: IO ()
+main = do
+  let monkeys :: [Monkey] = getExampleMonkeys
+  print (fmap showMonkeyTurn monkeys)
+  print "-----------------"
+  let turns :: [[Monkey]] = solveIt monkeys
+  let results = fmap (fmap showMonkeyTurn) turns
+  print results
---
+++ 2022/app/Day11_example_1.hs
@@ -0,0 +1,117 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Day11_example_1 (Monkey(..), getExampleMonkeys) where
+
+type WorryLevel = Int
+
+data Monkey = Monkey {
+  monkeyIndex :: Int,
+  startingItems :: [WorryLevel],
+  operation :: (Int -> Int),
+  test :: (Int -> Bool),
+  throwToIfTrue :: Int,
+  throwToIfFalse :: Int
+}
+
+getExampleMonkeys :: [Monkey]
+getExampleMonkeys = [
+  Monkey {
+    monkeyIndex = 0,
+    startingItems = [79, 98],
+    operation = (* 19),
+    test = (\x -> x `mod` 23 == 0),
+    throwToIfTrue = 2,
+    throwToIfFalse = 3
+  },
+  Monkey {
+    monkeyIndex = 1,
+    startingItems = [54, 65, 75, 74],
+    operation = (+ 6),
+    test = (\x -> x `mod` 19 == 0),
+    throwToIfTrue = 2,
+    throwToIfFalse = 0
+  },
+  Monkey {
+    monkeyIndex = 2,
+    startingItems = [79, 60, 97],
+    operation = (\x -> x * x),
+    test = (\x -> x `mod` 13 == 0),
+    throwToIfTrue = 1,
+    throwToIfFalse = 3
+  },
+  Monkey {
+    monkeyIndex = 3,
+    startingItems = [74],
+    operation = (+ 3),
+    test = (\x -> x `mod` 17 == 0),
+    throwToIfTrue = 0,
+    throwToIfFalse = 1
+  }
+  ]
+
+getInputMonkeys :: [Monkey]
+getInputMonkeys = [
+    Monkey {
+      monkeyIndex = 0,
+      startingItems = [52, 60, 85, 69, 75, 75],
+      operation = (* 17),
+      test = (\x -> x `mod` 13 == 0),
+      throwToIfTrue = 6,
+      throwToIfFalse = 7
+    },
+    Monkey {
+      monkeyIndex = 1,
+      startingItems = [96, 82, 61, 99, 82, 84, 85],
+      operation = (+ 8),
+      test = (\x -> x `mod` 7 == 0),
+      throwToIfTrue = 0,
+      throwToIfFalse = 7
+    },
+    Monkey {
+      monkeyIndex = 2,
+      startingItems = [95, 79],
+      operation = (+ 6),
+      test = (\x -> x `mod` 19 == 0),
+      throwToIfTrue = 5,
+      throwToIfFalse = 3
+    },
+    Monkey {
+      monkeyIndex = 3,
+      startingItems = [88, 50, 82, 65, 77],
+      operation = (* 19),
+      test = (\x -> x `mod` 2 == 0),
+      throwToIfTrue = 4,
+      throwToIfFalse = 1
+    },
+    Monkey {
+      monkeyIndex = 4,
+      startingItems = [66, 90, 59, 90, 87, 63, 53, 88],
+      operation = (+ 7),
+      test = (\x -> x `mod` 5 == 0),
+      throwToIfTrue = 1,
+      throwToIfFalse = 0
+    },
+    Monkey {
+      monkeyIndex = 5,
+      startingItems = [92, 75, 62],
+      operation = (\x -> x * x),
+      test = (\x -> x `mod` 3 == 0),
+      throwToIfTrue = 3,
+      throwToIfFalse = 4
+    },
+    Monkey {
+      monkeyIndex = 6,
+      startingItems = [94, 86, 76, 67],
+      operation = (+ 1),
+      test = (\x -> x `mod` 11 == 0),
+      throwToIfTrue = 5,
+      throwToIfFalse = 2
+    },
+    Monkey {
+      monkeyIndex = 7,
+      startingItems = [57],
+      operation = (+ 2),
+      test = (\x -> x `mod` 17 == 0),
+      throwToIfTrue = 6,
+      throwToIfFalse = 2
+    }
+    ]
