3d1f66a36f712438f4a5e8fd95651e4817f264f0
Niklas Leopold <leopold.niklas@gmail.com>
2024-10-17 14:29:34 +0200

    Day24

--- 2020/Day24/app/Main.hs
+++ 2020/Day24/app/Main.hs
@@ -7,20 +7,8 @@ import qualified Data.Map as Map
 
 main :: IO ()
 main = do
-  putStrLn $ show $ getPath "nwnwneseeswswnenewneswwnewseswneseene"
-  putStrLn $ show $ reverse $ traversePath (2,2) $ getPath "nwnwneseeswswnenewneswwnewseswneseene"
-  putStrLn ""
 
-  putStrLn $ show $ getPath "nenewswnwewswnenesenwnesewesw"
-  putStrLn $ show $ reverse $ traversePath (4,4) $ getPath "nenewswnwewswnenesenwnesewesw"
-  putStrLn ""
-
-  putStrLn $ show $ getPath "eneswnwswnwsenenwnwnwwseeswneewsenese"
-  putStrLn $ show $ reverse $ traversePath (2,2) $ getPath "eneswnwswnwsenenwnwnwwseeswneewsenese"
-  putStrLn ""
-
-
-  content <- readFile "/Users/niklasleopold/workspace/advent_of_Code/2020/Day24/app/example.txt"
+  content <- readFile "/Users/niklasleopold/workspace/advent_of_Code/2020/Day24/app/input.txt"
   let linesOfFile = lines content
 
   let allDiscoveredTiles = map (\line -> head $ traversePath (0, 0) (getPath line)) linesOfFile
@@ -31,6 +19,10 @@ main = do
   let tileVisits = countOccurrences allDiscoveredTiles
 
   mapM_ (\(tile, count) -> putStrLn $ show tile ++ " visited " ++ show count ++ " times") (Map.toList tileVisits)
+
+  let blackTiles = filter (\(_, count) -> odd count) (Map.toList tileVisits)
+
+  putStrLn $ show (length blackTiles)
 
 countOccurrences :: (Ord k) => [k] -> Map.Map k Int
 countOccurrences = foldr (\key acc -> Map.insertWith (+) key 1 acc) Map.empty
