8ee0dd5646059b9cece6957b48f472956f6e1c88
Niklas Leopold <leopold.niklas@gmail.com>
2024-10-18 19:00:49 +0200

    Day25

--- 2020/Day25/app/Challenge.hs
+++ 2020/Day25/app/Challenge.hs
@@ -1,5 +1,38 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-module Challenge(hello) where
+module Challenge (transformSubjectNumber, getLoopNumber) where
 
-hello :: String
-hello = "hello world!"
+import qualified Data.Map as Map
+import Debug.Trace (trace)
+
+type MemoCache = Map.Map Integer Integer
+
+transformSubjectNumber :: Integer -> Integer -> MemoCache -> (Integer, MemoCache)
+transformSubjectNumber 0 subjectNumber cache = (subjectNumber, cache)
+transformSubjectNumber loopSize subjectNumber cache =
+    case Map.lookup loopSize cache of
+        Just result -> (result, cache)
+        Nothing     ->
+            let (nextResult, newCache) = transform (loopSize - 1) subjectNumber cache
+                result = rem (nextResult * subjectNumber) 20201227
+            in (result, Map.insert loopSize result newCache)
+  where
+    transform :: Integer -> Integer -> MemoCache -> (Integer, MemoCache)
+    transform 0 _ cache = (1, cache)
+    transform loopSize subjectNumber cache =
+        case Map.lookup loopSize cache of
+            Just value -> (value, cache)
+            Nothing ->
+                let (nextValue, updatedCache) = transform (loopSize - 1) subjectNumber cache
+                    newValue = rem (nextValue * subjectNumber) 20201227
+                in (newValue, Map.insert loopSize newValue updatedCache)
+
+getLoopNumber :: Integer -> Integer -> Integer
+getLoopNumber publicKey subjectNumber =
+    getLoopNumber' 1 Map.empty
+  where
+    getLoopNumber' :: Integer -> MemoCache -> Integer
+    getLoopNumber' loopSize cache =
+        let (transformed, newCache) = transformSubjectNumber loopSize subjectNumber cache
+        in if transformed == publicKey
+            then loopSize
+            else getLoopNumber' (loopSize + 1) newCache
--- 2020/Day25/app/Main.hs
+++ 2020/Day25/app/Main.hs
@@ -3,4 +3,19 @@ module Main where
 import Challenge
 
 main :: IO ()
-main = putStrLn hello
+main = do
+  -- Extract the result from transformSubjectNumber (ignoring the cache)
+  let (result1, _) = transformSubjectNumber 8 7 mempty
+  putStrLn $ show result1
+
+  putStrLn $ show $ getLoopNumber 5764801 7
+  putStrLn $ show $ getLoopNumber 17807724 7
+
+  let (result2, _) = transformSubjectNumber 8 17807724 mempty
+  putStrLn $ show result2
+
+  putStrLn $ show $ getLoopNumber 1614360 7
+  putStrLn $ show $ getLoopNumber 7734663 7
+
+  let (result3, _) = transformSubjectNumber 1182212 7734663 mempty
+  putStrLn $ show result3
