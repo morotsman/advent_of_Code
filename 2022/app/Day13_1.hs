91eee312788a4f88b11b5ffd6f13d0b252f8e0a5
Niklas Leopold <leopold.niklas@gmail.com>
2024-12-22 09:17:29 +0100

    day 13 init

---
+++ 2022/app/Day13_1.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+type Answer = [String]
+
+solveIt :: [String] -> Answer
+solveIt input = input
+
+main :: IO ()
+main = do
+  content <- readFile "/Users/niklasleopold/workspace/advent_of_Code/2022/app/Day13_example.txt"
+  let linesOfFile = lines content
+  let answer = solveIt linesOfFile
+  print answer
